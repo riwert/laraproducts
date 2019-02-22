46368448bc9e2b6a3e9c683b58f962c676c8f196
Riwert <riwert@gmail.com>
2019-02-22 16:57:34 +0100

    fix favicon path

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 
         <title>@yield('title')</title>
     </head>

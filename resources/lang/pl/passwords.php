5448cf04b143bab47d943229bf463b1957e5c8f4
Riwert <riwert@gmail.com>
2019-03-01 11:29:02 +0100

    add translations

---
+++ resources/lang/pl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Hasło musi mieć co najmniej 6 znaków i pasować do potwierdzenia.',
+    'reset' => 'Twoje hasło zostało zresetowane!',
+    'sent' => 'Link resetujący hasło został wysłany na Twój e-mail.',
+    'token' => 'Token resetu hasła jest nieprawidłowy.',
+    'user' => "W bazie nie ma użytkownika o takim adresie e-mail.",
+
+];

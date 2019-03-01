576c02eed8823af393e2eb30a7bcd4fbe897d2eb
Riwert <riwert@gmail.com>
2019-03-01 11:45:31 +0100

    add required frontend validation

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -12,7 +12,7 @@
             <label for="email" class="col-md-4 control-label">{{ __('Adres e-mail') }}</label>
 
             <div class="col-md-6">
-                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
                 @if ($errors->has('email'))
                     <span class="help-block">

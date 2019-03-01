1e24a5e2fd34194586c89707a611399fc4c74bc9
Riwert <riwert@gmail.com>
2019-03-01 18:53:29 +0100

    add or old to prices

--- resources/views/products/prices-input.blade.php
+++ resources/views/products/prices-input.blade.php
@@ -4,9 +4,9 @@
         @foreach($product->prices as $price)
             <li class="d-flex justify-content-between mb-2">
                 <input type="hidden" name="prices[{{ $loop->iteration }}][id]" value="{{ $price->id }}">
-                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][name]" value="{{ $price->name }}" placeholder="{{ __('Nazwa ceny') }}" required="required">
-                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][value]" value="{{ $price->value }}" placeholder="{{ __('Wartość ceny') }}" required="required">
-                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][unit]" value="{{ $price->unit }}" placeholder="{{ __('Jednostka ceny') }}" required="required">
+                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][name]" value="{{ $price->name or old('prices')[$loop->iteration]['name'] }}" placeholder="{{ __('Nazwa ceny') }}" required="required">
+                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][value]" value="{{ $price->value or old('prices')[$loop->iteration]['value'] }}" placeholder="{{ __('Wartość ceny') }}" required="required">
+                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][unit]" value="{{ $price->unit or old('prices')[$loop->iteration]['unit'] }}" placeholder="{{ __('Jednostka ceny') }}" required="required">
                 <a class="price-add btn btn-primary mx-1" href="#add" title="{{ __('Dodaj cenę') }}">{{ __('+') }}</a>
                 <a class="price-delete btn btn-danger mx-1" href="#delete" title="{{ __('Usuń cenę') }}">{{ __('-') }}</a>
             </li>

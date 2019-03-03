58b4dec442387fe70cc8b8feda2c61c4497f620c
Riwert <riwert@gmail.com>
2019-03-03 13:03:08 +0100

    fix old prices id, clean up code

--- resources/views/products/prices-input.blade.php
+++ resources/views/products/prices-input.blade.php
@@ -1,23 +1,25 @@
 <h3>{{ __('Ceny') }}</h3>
 <ul class="product-price-list p-0">
-    @if (old('prices') !== null)
-        @foreach(old('prices') as $id => $price)
+    @if (old('prices'))
+        @foreach(old('prices') as $price)
             <li class="d-flex justify-content-between mb-2">
-                <input type="hidden" name="prices[{{ $loop->iteration }}][id]" value="{{ $id }}">
-                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][name]" value="{{ $price['name'] or old('prices')[$loop->iteration]['name'] }}" placeholder="{{ __('Nazwa ceny') }}" required>
-                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][value]" value="{{ $price['value'] or old('prices')[$loop->iteration]['value'] }}" placeholder="{{ __('Wartość ceny') }}" required>
-                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][unit]" value="{{ $price['unit'] or old('prices')[$loop->iteration]['unit'] }}" placeholder="{{ __('Jednostka ceny') }}" required>
+                @if (isset($price['id']))
+                    <input type="hidden" name="prices[{{ $loop->iteration }}][id]" value="{{ $price['id'] }}">
+                @endif
+                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][name]" value="{{ $price['name'] }}" placeholder="{{ __('Nazwa ceny') }}" required>
+                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][value]" value="{{ $price['value'] }}" placeholder="{{ __('Wartość ceny') }}" required>
+                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][unit]" value="{{ $price['unit'] }}" placeholder="{{ __('Jednostka ceny') }}" required>
                 <a class="price-add btn btn-primary mx-1" href="#add" title="{{ __('Dodaj cenę') }}">{{ __('+') }}</a>
                 <a class="price-delete btn btn-danger mx-1" href="#delete" title="{{ __('Usuń cenę') }}">{{ __('-') }}</a>
             </li>
         @endforeach
-    @elseif (isset($product) && $product->prices->count())
+    @elseif (isset($product->prices) && $product->prices->count())
         @foreach($product->prices as $price)
             <li class="d-flex justify-content-between mb-2">
                 <input type="hidden" name="prices[{{ $loop->iteration }}][id]" value="{{ $price->id }}">
-                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][name]" value="{{ $price->name or old('prices')[$loop->iteration]['name'] }}" placeholder="{{ __('Nazwa ceny') }}" required>
-                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][value]" value="{{ $price->value or old('prices')[$loop->iteration]['value'] }}" placeholder="{{ __('Wartość ceny') }}" required>
-                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][unit]" value="{{ $price->unit or old('prices')[$loop->iteration]['unit'] }}" placeholder="{{ __('Jednostka ceny') }}" required>
+                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][name]" value="{{ $price->name }}" placeholder="{{ __('Nazwa ceny') }}" required>
+                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][value]" value="{{ $price->value }}" placeholder="{{ __('Wartość ceny') }}" required>
+                <input class="form-control mx-1" type="text" name="prices[{{ $loop->iteration }}][unit]" value="{{ $price->unit }}" placeholder="{{ __('Jednostka ceny') }}" required>
                 <a class="price-add btn btn-primary mx-1" href="#add" title="{{ __('Dodaj cenę') }}">{{ __('+') }}</a>
                 <a class="price-delete btn btn-danger mx-1" href="#delete" title="{{ __('Usuń cenę') }}">{{ __('-') }}</a>
             </li>
